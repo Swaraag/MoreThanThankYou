f24fe5e4042d79152169344be79da89cda4290d2
ThePhoenix10 <saivellanki10@gmail.com>
2024-03-31 15:41:29 -0700

    phone

--- static/index.js
+++ static/index.js
@@ -1,19 +1,14 @@
-
 document.addEventListener('DOMContentLoaded', function () {
-
     const accountName = document.getElementById("name");
-    
+
+    // Fetch account name from server
     fetch('/checkSession')
-            .then(response => response.json())
-            .then(data => {
-                // Clear existing user list
-                console.log(data)
+        .then(response => response.json())
+        .then(data => {
+            accountName.textContent = data.message;
+        })
+        .catch(error => console.error('Error:', error));
 
-                accountName.textContent = data.message
-
-            })
-            .catch(error => console.error('Error:', error));
-    
     var intervalEnabled = true;
 
     document.getElementById("left").addEventListener("click", function() {
@@ -26,13 +21,13 @@ document.addEventListener('DOMContentLoaded', function () {
             intervalEnabled = false;
         }, 5000);
     });
-    
+
     document.getElementById("right").addEventListener("click", function() {
         intervalEnabled = false;
         var currentSlide = document.querySelector('input[name="slider"]:checked');
         var nextSlide = currentSlide.nextElementSibling || document.querySelector('input[name="slider"]:first-of-type');
-        
-        if (currentSlide.id === "slide4") {
+
+        if (currentSlide.id === "slide3") {
             document.getElementById("slide1").checked = true;
         } else {
             currentSlide.checked = false;
@@ -42,24 +37,21 @@ document.addEventListener('DOMContentLoaded', function () {
             intervalEnabled = true;
         }, 10000);
     });
-    
+
     function nextSlide() {
         if (!intervalEnabled) return;
         var currentSlide = document.querySelector('input[name="slider"]:checked');
         var nextSlide = currentSlide.nextElementSibling || document.querySelector('input[name="slider"]:first-of-type');
-        
-        if (currentSlide.id === "slide4") {
+
+        if (currentSlide.id === "slide3") {
             document.getElementById("slide1").checked = true;
         } else {
             currentSlide.checked = false;
             nextSlide.checked = true;
         }
     }
-    
-    setInterval(nextSlide, 5000);
-    
-    
-            
-            
-    });
 
+    setInterval(nextSlide, 5000);
+});
+
+  
