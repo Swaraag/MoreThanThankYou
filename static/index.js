304893f2966887d062b2a7d076158854cb3acb67
ThePhoenix10 <saivellanki10@gmail.com>
2024-04-04 23:04:35 -0700

    final

--- static/index.js
+++ static/index.js
@@ -9,21 +9,15 @@ document.addEventListener('DOMContentLoaded', function () {
         })
         .catch(error => console.error('Error:', error));
 
-    var intervalEnabled = true;
 
     document.getElementById("left").addEventListener("click", function() {
-        intervalEnabled = false;
         var currentSlide = document.querySelector('input[name="slider"]:checked');
         var prevSlide = currentSlide.previousElementSibling || document.querySelector('input[name="slider"]:last-of-type');
         currentSlide.checked = false;
         prevSlide.checked = true;
-        setTimeout(function() {
-            intervalEnabled = false;
-        }, 5000);
     });
 
     document.getElementById("right").addEventListener("click", function() {
-        intervalEnabled = false;
         var currentSlide = document.querySelector('input[name="slider"]:checked');
         var nextSlide = currentSlide.nextElementSibling || document.querySelector('input[name="slider"]:first-of-type');
 
@@ -33,13 +27,9 @@ document.addEventListener('DOMContentLoaded', function () {
             currentSlide.checked = false;
             nextSlide.checked = true;
         }
-        setTimeout(function() {
-            intervalEnabled = true;
-        }, 10000);
     });
 
     function nextSlide() {
-        if (!intervalEnabled) return;
         var currentSlide = document.querySelector('input[name="slider"]:checked');
         var nextSlide = currentSlide.nextElementSibling || document.querySelector('input[name="slider"]:first-of-type');
 
